8ad2b7b1024a495fbedc2d714f585e421fe58415
Peter Somers <psomers3@gatech.edu>
2019-02-11 12:36:50 +0100

    cmake file set up for test.cpp

---
+++ test/test.cpp
@@ -0,0 +1,16 @@
+//
+//  test.cpp
+//  NeuralNet
+//
+//  Created by Peter Somers on 2/11/19.
+//  Copyright © 2019 Peter Somers. All rights reserved.
+//
+
+#include "Brain.hpp"
+
+int main()
+{
+    Brain brain({4,5,6,3});
+    
+    return 0;
+}
